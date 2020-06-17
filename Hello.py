d41c63e5eb5e6e466ef373b4d0bfe52c11142dee
Keon <rjsdlwwkd@kaist.ac.kr>
2020-06-18 00:58:05 +0900

    init Hello.py for tensorflow-gpu

--- Hello.py
+++ Hello.py
@@ -1,4 +1,9 @@
-from __future__ import print_function
-import torch
-x = torch.rand(5, 3)
-print(x)
+import tensorflow as tf
+from tensorflow import keras
+print(tf.__version__)
+print(keras.__version__)
+
+# from __future__ import print_function
+# import torch
+# x = torch.rand(5, 3)
+# print(x)
